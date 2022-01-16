3910a088f1c6b865b5106cd8b5fa3cc57d0bee03
autor00t <54729539+autor00t@users.noreply.github.com>
2022-01-16 17:59:00 -0300

    Update minimalGraphical.sh

--- minimalGraphical.sh
+++ minimalGraphical.sh
@@ -9,4 +9,11 @@ sudo systemctl start NetworkManager
 
 sudo cp /etc/X11/xinit/xinitrc ~/.xinitrc
 echo "Remember to modify the .xinitrc file, exec startplasma-x11"
+
+echo "Installing TLP...."
+sudo pacman -S awk grep sed ethtool hdparm iw pciutils rfkill smartmontools udev usbutils util-linux
+sudo pacman -S tlp
+sudo systemctl enable tlp.service
+sudo systemctl start tlp.service
+
 echo "Done"
