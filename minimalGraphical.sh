eede1b42c251a41beec3d0013a170977e30018ad
autor00t <54729539+autor00t@users.noreply.github.com>
2022-01-16 17:33:47 -0300

    Create minimalGraphical.sh
    I install a minimal version of KDE Plasma while also installing xorg and intel graphics drivers.

---
+++ minimalGraphical.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+echo "Remember to make multilib available"
+
+sudo pacman -Syuu
+sudo pacman -S mesa lib32-mesa xorg xorg-xinit plasma-desktop
+sudo pacman -S firefox plasma-nm plasma-pa dolphin konsole kdeplasma-addons kde-gtk-config ark powerdevil
+sudo systemctl enable NetworkManager
+sudo systemctl start NetworkManager
+
+sudo cp /etc/X11/xinit/xinitrc ~/.xinitrc
+echo "Remember to modify the .xinitrc file, exec startplasma-x11"
