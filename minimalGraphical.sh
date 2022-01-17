767b3c0937590163ac98210ae67a81fcb7dadbcb
autor00t <54729539+autor00t@users.noreply.github.com>
2022-01-17 15:55:03 -0300

    Update minimalGraphical.sh

--- minimalGraphical.sh
+++ minimalGraphical.sh
@@ -5,6 +5,8 @@ sudo pacman -Syuu
 sudo pacman -S mesa lib32-mesa xorg xorg-xinit plasma-desktop
 sudo pacman -S firefox plasma-nm plasma-pa dolphin alacritty kdeplasma-addons kde-gtk-config ark powerdevil neofetch
 sudo pacman -S kuickshow okular xdg-utils vlc
+sudo pacman -S noto-fonts-cjk noto-fonts-emoji noto-fonts ttf-liberation
+yay -S ttf-freefont ttf-ms-fonts ttf-linux-libertine ttf-dejavu ttf-inconsolata ttf-ubuntu-font-family
 yay -S fim
 sudo systemctl enable NetworkManager
 sudo systemctl start NetworkManager
