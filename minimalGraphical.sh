d5ef6906081493090f0b0ec152cb930f5199cdb3
autor00t <54729539+autor00t@users.noreply.github.com>
2022-01-18 00:22:47 -0300

    Update minimalGraphical.sh

--- minimalGraphical.sh
+++ minimalGraphical.sh
@@ -2,7 +2,7 @@
 echo "Remember to enable multilib"
 
 sudo pacman -Syuu
-sudo pacman -S xorg xorg-xinit plasma-desktop
+sudo pacman -S xorg xorg-xinit plasma-desktop linux-headers
 sudo pacman -S firefox plasma-nm plasma-pa dolphin alacritty kdeplasma-addons kde-gtk-config ark powerdevil neofetch
 sudo pacman -S kuickshow okular xdg-utils vlc
 sudo pacman -S noto-fonts-cjk noto-fonts-emoji noto-fonts ttf-liberation
