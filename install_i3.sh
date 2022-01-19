4038e462cf883e2e0b503f8303abb215336a38c4
autor00t <54729539+autor00t@users.noreply.github.com>
2022-01-19 04:07:08 -0300

    Update install_i3.sh

--- install_i3.sh
+++ install_i3.sh
@@ -5,6 +5,6 @@ sudo pacman -Syuu
 sudo pacman -S xorg xorg-xinit
 sudo cp /etc/X11/xinit/xinitrc ~/.xinitrc
 
-sudo pacman -S i3-gaps i3lock i3status ttf-dejavu i3blocks dmenu alacritty chromium alsa-utils
+sudo pacman -S i3-gaps i3lock i3status ttf-dejavu i3blocks dmenu alacritty chromium
 
 echo "Done, please work...."
