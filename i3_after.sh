4ac03b5807f1656b692271469bd93713bf388458
autor00t <54729539+autor00t@users.noreply.github.com>
2022-01-19 04:06:50 -0300

    Create i3_after.sh

---
+++ i3_after.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo setxkbmap latam
+
+# Just one command xd, but for real, remember to edit i3 config file and put:
+# # Keyboard Layout
+# exec_always setxkbmap latam
+
+# also change the movement keys..... yeap.
