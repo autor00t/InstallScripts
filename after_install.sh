47a845a2c8473e3f6d3418e55c9c542562f58bce
autor00t <54729539+autor00t@users.noreply.github.com>
2022-01-21 02:54:08 -0300

    Delete after_install.sh

--- after_install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-pacman -S  network-manager-applet dialog wpa_supplicant base-devel linux-headers bluez bluez-utils xdg-utils xdg-user-dirs pulseaudio alsa-utils
-systemctl enable bluetooth
-systemctl enable fstrim.timer
-
-# Once you install nvidia drivers remember to modify /etc/mkinitcpio.conf
