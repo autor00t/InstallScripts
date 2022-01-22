5cf6749afab30e26d8e0cdd578ce8571341f535e
autor00t <54729539+autor00t@users.noreply.github.com>
2022-01-22 19:04:37 -0300

    Update install_tlp.sh

--- install_tlp.sh
+++ install_tlp.sh
@@ -2,7 +2,8 @@
 
 echo "Installing TLP...."
 sudo pacman -S awk grep sed ethtool hdparm iw pciutils rfkill smartmontools udev usbutils util-linux
-sudo pacman -S tlp tlpui
+sudo pacman -S tlp
+yay -S tlpui
 sudo systemctl enable tlp.service
 sudo systemctl start tlp.service
 
