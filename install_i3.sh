20185995c8693ea977517c82a8a0c37e42d59e73
autor00t <54729539+autor00t@users.noreply.github.com>
2022-01-25 00:20:35 -0300

    Update install_i3.sh

--- install_i3.sh
+++ install_i3.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 sudo pacman -Syuu
-sudo pacman -S network-manager-applet dialog wpa_supplicant bluez bluez-utils xdg-user-dirs pulseaudio pavucontrol alsa alsa-utils sxiv
+sudo pacman -S network-manager-applet dialog wpa_supplicant bluez bluez-utils xdg-user-dirs pulseaudio pavucontrol alsa alsa-utils sxiv nitrogen
 sudo pacman -S xorg xorg-xinit
 sudo cp /etc/X11/xinit/xinitrc ~/.xinitrc
 
-sudo pacman -S i3-gaps i3lock i3status ttf-dejavu i3blocks dmenu alacritty firefox
+sudo pacman -S i3-gaps i3lock i3status ttf-dejavu i3blocks rofi kitty firefox
 
 echo "Done, please work...."
