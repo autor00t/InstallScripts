7e9a5e582a4585bdf10cada3740069dffec51c9b
autor00t <54729539+autor00t@users.noreply.github.com>
2022-01-27 23:06:05 -0300

    Update install_i3.sh

--- install_i3.sh
+++ install_i3.sh
@@ -6,5 +6,9 @@ sudo pacman -S xorg xorg-xinit
 sudo cp /etc/X11/xinit/xinitrc ~/.xinitrc
 
 sudo pacman -S i3-gaps i3lock i3status ttf-dejavu i3blocks dmenu kitty firefox lxsession
+sudo pacman -S cups cups-pdf gnome-screenshot thunar thunar-archive-plugin
+sudo systemctl enable cups.socket
+sudo systemctl start cups.socket
+echo "Remember to add the cups-pdf printer"
 
 echo "Done, please work...."
