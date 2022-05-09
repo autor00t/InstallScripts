364f3682549619cfc62cc6756ddb1d41f5cd9c53
autor00t <54729539+autor00t@users.noreply.github.com>
2022-05-09 19:05:42 -0400

    Update install_i3++.sh

--- install_i3++.sh
+++ install_i3++.sh
@@ -5,8 +5,8 @@ sudo pacman -S network-manager-applet dialog wpa_supplicant bluez bluez-utils xd
 sudo pacman -S xorg xorg-xinit
 sudo cp /etc/X11/xinit/xinitrc ~/.xinitrc
 
-sudo pacman -S i3-gaps i3lock i3status ttf-dejavu polybar dmenu kitty opera lxsession pfetch neofetch vlc
-sudo pacman -S cups cups-pdf thunar thunar-archive-plugin
+sudo pacman -S i3-gaps i3lock i3status ttf-dejavu polybar dmenu kitty opera opera-ffmpeg-codecs lxsession pfetch neofetch vlc parted gparted
+sudo pacman -S cups cups-pdf thunar thunar-archive-plugin ark
 sudo systemctl enable cups.socket
 sudo systemctl start cups.socket
 echo "Remember to add the cups-pdf printer"
@@ -15,8 +15,7 @@ sudo pacman -S noto-fonts-cjk noto-fonts-emoji noto-fonts ttf-liberation
 yay -S ttf-freefont ttf-ms-fonts ttf-linux-libertine ttf-dejavu ttf-inconsolata ttf-ubuntu-font-family
 
 #wqy-zenhei is for Asian languages
-sudo pacman -S discord steam wqy-zenhei
-yay -S spotify picom-git code
-
+sudo pacman -S discord steam wqy-zenhei fuse ntfs-3g udisks2 udiskie code
+yay -S spotify picom-git autotiling
 
 echo "Done, please work...."
