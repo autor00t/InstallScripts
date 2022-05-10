904d6bdfb5c2f36289c5dacd42e5d17eeb60681e
autor00t <54729539+autor00t@users.noreply.github.com>
2022-05-09 23:43:11 -0400

    Update install_i3++.sh

--- install_i3++.sh
+++ install_i3++.sh
@@ -6,6 +6,7 @@ sudo pacman -S xorg xorg-xinit
 sudo cp /etc/X11/xinit/xinitrc ~/.xinitrc
 
 sudo pacman -S i3-gaps i3lock i3status ttf-dejavu polybar dmenu kitty opera opera-ffmpeg-codecs lxsession pfetch neofetch vlc parted gparted
+sudo pacman -S thunderbird thunderbird-i18n-es-es
 sudo pacman -S cups cups-pdf thunar thunar-archive-plugin ark
 sudo systemctl enable cups.socket
 sudo systemctl start cups.socket
