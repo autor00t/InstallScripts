fe210b75346f53fca70cdbfe21e699da091c288c
autor00t <54729539+autor00t@users.noreply.github.com>
2022-05-11 22:39:40 -0400

    Update install_i3++.sh

--- install_i3++.sh
+++ install_i3++.sh
@@ -5,7 +5,8 @@ sudo pacman -S network-manager-applet dialog wpa_supplicant bluez bluez-utils xd
 sudo pacman -S xorg xorg-xinit
 sudo cp /etc/X11/xinit/xinitrc ~/.xinitrc
 
-sudo pacman -S i3-gaps i3lock i3status ttf-dejavu polybar dmenu kitty opera opera-ffmpeg-codecs lxsession pfetch neofetch vlc parted gparted
+sudo pacman -S i3-gaps i3lock i3status ttf-dejavu polybar dmenu kitty opera opera-ffmpeg-codecs lxsession neofetch vlc parted gparted
+yay -S pfetch
 sudo pacman -S thunderbird thunderbird-i18n-es-es
 sudo pacman -S cups cups-pdf thunar thunar-archive-plugin ark
 sudo systemctl enable cups.socket
