23ecbae519e1f3573863b822c5e32a343a56707d
autor00t <54729539+autor00t@users.noreply.github.com>
2022-05-17 14:50:07 -0400

    Delete i3_after.sh

--- i3_after.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-sudo setxkbmap latam
-
-# Just one command xd, but for real, remember to edit i3 config file and put:
-# # Keyboard Layout
-# exec_always setxkbmap latam
-
-# also change the movement keys..... yeap.
